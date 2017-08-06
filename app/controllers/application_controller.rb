3c6981d6e6062aaae17909d8355eae5dc425aae7
Brad Gessler <bradgessler@gmail.com>
2017-08-05 17:22:39 -0700

    Include Pundit in rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  private
+  def current_user=(user)
+    session["user_id"] = user.id
+  end
+
+  def current_user
+    User.find session["user_id"]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :email, presence: true
+
+  # TODO: Move this into a service object.
   def self.find_or_create_from_auth_hash(auth_hash)
     find_or_create_by! email: auth_hash.info.email
   end
