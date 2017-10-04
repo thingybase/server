7296dd22146b0d4f2538b525bcb8f25c2293f975
Brad Gessler <bradgessler@gmail.com>
2017-10-03 19:21:44 -0700

    Remove unused tables from schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,20 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20171001222900) do
 
-  create_table "invitations", force: :cascade do |t|
-    t.string "token"
-    t.integer "ttl"
-    t.integer "user_id"
-    t.integer "team_id"
-    t.string "message"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["team_id"], name: "index_invitations_on_team_id"
-    t.index ["token"], name: "index_invitations_on_token"
-    t.index ["user_id"], name: "index_invitations_on_user_id"
-  end
-
   create_table "members", force: :cascade do |t|
     t.integer "user_id"
     t.integer "team_id"
@@ -51,15 +37,6 @@ ActiveRecord::Schema.define(version: 20171001222900) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_teams_on_user_id"
-  end
-
-  create_table "teams_users", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "team_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["team_id"], name: "index_teams_users_on_team_id"
-    t.index ["user_id"], name: "index_teams_users_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
