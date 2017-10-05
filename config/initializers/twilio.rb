e8bc57307eaeda55f539072d04a2431dd5e8e11a
Brad Gessler <bradgessler@gmail.com>
2017-10-05 00:39:40 -0700

    Nil defaults for Twilio keys

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,5 +1,5 @@
 # Configure Twilio servive
 Twilio.configure do |config|
-  config.auth_token = ENV.fetch("TWILIO_AUTH_TOKEN")
-  config.account_sid = ENV.fetch("TWILIO_ACCOUNT_SID")
+  config.auth_token = ENV.fetch("TWILIO_AUTH_TOKEN", nil)
+  config.account_sid = ENV.fetch("TWILIO_ACCOUNT_SID", nil)
 end
