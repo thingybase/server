d80bde91b03b3badb6aa7004e5ce58ffcd6f3de3
Brad Gessler <bradgessler@gmail.com>
2017-10-05 00:51:28 -0700

    Set production default host options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.default_url_options = { host: 'www.pagerline.com' }
+  routes.default_url_options = { host: 'www.pagerline.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
