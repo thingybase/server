30ecce7918c63d6058b2e2882bccda80e59dfde8
Brad Gessler <bradgessler@gmail.com>
2018-03-21 02:51:08 -0700

    Change forgery protection so APIs work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,12 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: [:index]
   after_action :verify_policy_scoped, only: :index
 
-  protect_from_forgery with: :exception
+  # TODO: Is this secure? I think I need to nullify the session for API calls.
+  protect_from_forgery with: :exception, unless: :has_authentication_header?
+  protect_from_forgery with: :null_session, if: :has_authentication_header?
+
+  private
+    def has_authentication_header?
+      request.headers.key? "Authentication"
+    end
 end
