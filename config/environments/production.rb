a558f5a7482806aac78860855e9f8a8a8536ed5a
Brad Gessler <bradgessler@gmail.com>
2018-03-28 02:58:19 -0700

    Use AWS for email delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,12 +3,12 @@ Rails.application.configure do
   routes.default_url_options = { host: 'www.pagerline.com' }
 
   config.action_mailer.smtp_settings = {
-    port:            ENV['MAILGUN_SMTP_PORT'],
-    address:         ENV['MAILGUN_SMTP_SERVER'],
-    user_name:       ENV['MAILGUN_SMTP_LOGIN'],
-    password:        ENV['MAILGUN_SMTP_PASSWORD'],
-    domain:          'www.pagerline.com',
-    authentication:  :plain,
+    address:         ENV['SMTP_SERVER'],
+    user_name:       ENV['SMTP_LOGIN'],
+    password:        ENV['SMTP_PASSWORD'],
+    port:            587,
+    authentication: :login,
+    enable_starttls_auto: true
   }
   config.action_mailer.delivery_method = :smtp
 
