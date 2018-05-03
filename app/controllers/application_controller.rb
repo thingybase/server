5aa33e69b31bc5a35ba7cedf6b485589c5cf5d41
Brad Gessler <bradgessler@gmail.com>
2018-05-02 23:29:47 -0700

    Make 404 and 403 errors prettier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,34 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, unless: :has_authentication_header?
   protect_from_forgery with: :null_session, if: :has_authentication_header?
 
+  rescue_from Pundit::NotAuthorizedError, with: :request_unauthorized
+  rescue_from ActiveRecord::RecordNotFound, with: :request_not_found
+
+  protected
+    def request_unauthorized(exception)
+      @permission = humanized_permission exception.query
+      render "unauthorized", layout: "application", status: :unauthorized
+    end
+
+    def request_not_found
+      render "not_found", layout: "application", status: :not_found
+    end
+
   private
     def has_authentication_header?
       request.headers.key? "Authentication"
+    end
+
+    def humanized_permission(query)
+      case query
+      when "show?"
+        "view"
+      when "update?", "edit?"
+        "edit"
+      when "create?", "new?"
+        "create"
+      else
+        "access"
+      end
     end
 end
