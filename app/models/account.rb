7862e49244bbefb7cae2017a080e2eb1f5cc1dc8
Brad Gessler <bradgessler@gmail.com>
2019-02-12 01:15:45 -0800

    Crappy dashboard

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,6 +4,8 @@ class Account < ApplicationRecord
   has_many :users, through: :members, dependent: :destroy
   has_many :invitations, dependent: :destroy
   has_many :labels, dependent: :destroy
+  has_many :items, dependent: :destroy
+  has_many :containers, dependent: :destroy
 
   validates :user, presence: true
   validates :name, presence: true
