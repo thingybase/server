a6a24da21550d4a6e11be74037a2f3d15ae0e857
Brad Gessler <bradgessler@gmail.com>
2019-02-20 22:09:37 -0800

    Add breadcrumbs to items and containers

---
+++ app/helpers/breadcrumb_helper.rb
@@ -0,0 +1,34 @@
+module BreadcrumbHelper
+  Link = Struct.new(:text, :url)
+
+
+  def container_breadcrumbs(container=resource)
+    container.ancestors.map{ |c| Link.new(c.name, c) }
+      .append(Link.new(container.name, container))
+      .prepend(Link.new(@account.name, account_containers_path(@account)))
+  end
+
+  def item_breadcrumbs(item=resource)
+    if item.container
+      container_breadcrumbs(item.container).append Link.new(item.name, item)
+    else
+      []
+    end
+  end
+
+  def label_breadcrumbs(label=resource)
+    links = case label.labelable
+    when Item
+      item_breadcrumbs(label.labelable)
+    when Container
+      container_breadcrumbs(label.labelable)
+    end
+    links.append Link.new("Label", label)
+  end
+
+  def breadcrumb(links = [], text: nil)
+    return if links.empty?
+    root, *links, active = links
+    render partial: "helpers/breadcrumb_helper/breadcrumb", locals: { root: root, links: links, active: active }
+  end
+end
