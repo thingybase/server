301ae3b6b792dd1273119c1a37433c81e2c8aa86
Brad Gessler <bradgessler@gmail.com>
2019-02-23 00:07:49 -0800

    Show container parent of container in search result

--- app/controllers/accounts/searches_controller.rb
+++ app/controllers/accounts/searches_controller.rb
@@ -1,7 +1,11 @@
 class Accounts::SearchesController < ApplicationController
+  include AccountLayout
+
   before_action :authenticate_user
 
   def index
-    @search = Search.new(phrase: params[:search], items: policy_scope(Item), containers: policy_scope(Container))
+    @search = Search.new(phrase: params[:search],
+      items: policy_scope(Item).includes(:container),
+      containers: policy_scope(Container).includes(:parent))
   end
 end
