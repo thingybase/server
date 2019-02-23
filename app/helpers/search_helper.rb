8a3cec9b3860aa1f71b85dd7390cc130001ccdee
Brad Gessler <bradgessler@gmail.com>
2019-02-23 00:19:23 -0800

    Clean up search helper

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,8 +1,12 @@
 module SearchHelper
-  def search_field(url: nil, autofocus: nil)
+  def search_field(url: nil, autofocus: nil, value: nil)
     url ||= url_for
     autofocus ||= params[:search].present?
+    value ||= params[:search]
 
-    render partial: "helpers/search_helper/search_field", locals: { url: url, autofocus: autofocus }
+    render partial: "helpers/search_helper/search_field", locals: {
+      url: url,
+      autofocus: autofocus,
+      value: value }
   end
 end
