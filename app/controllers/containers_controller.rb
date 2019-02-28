2a228e5959b6b54704dfd15262d17eee40129a83
Brad Gessler <bradgessler@gmail.com>
2019-02-27 23:06:44 -0800

    Redirect container deletes to better paths

--- app/controllers/containers_controller.rb
+++ app/controllers/containers_controller.rb
@@ -5,6 +5,14 @@ class ContainersController < ResourcesController
     Container
   end
 
+  def destroy_redirect_url
+    if parent = @container.parent
+      parent
+    else
+      account_container_url(@container.account)
+    end
+  end
+
   def permitted_params
     [:name, :account_id, :parent_id]
   end
