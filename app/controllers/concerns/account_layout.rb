787c4786de0f36a9140d22071c448a597d57ecd7
Brad Gessler <bradgessler@gmail.com>
2019-06-30 01:19:49 -0400

    Handle nil @account in Container and Item new actions on failed validations

--- app/controllers/concerns/account_layout.rb
+++ app/controllers/concerns/account_layout.rb
@@ -8,15 +8,23 @@ module AccountLayout
 
   protected
     def set_account
-      @account = if params.key? :account_id
-        Account.find params[:account_id]
-      elsif resource
-        resource.account
-      end
+      @account = find_account_by_param || find_account_by_resource || find_account_by_resource_param
     end
 
   private
     def set_account_layout
       @account ? "account" : "application"
+    end
+
+    def find_account_by_param
+      Account.find params[:account_id] if params.key? :account_id
+    end
+
+    def find_account_by_resource_param
+      Account.find resource_params[:account_id] if params.key? resource_name
+    end
+
+    def find_account_by_resource
+      resource.account if resource
     end
 end
