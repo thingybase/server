44430e2a5e89e4ce70c0803f7dc8d020900a7940
Brad Gessler <bradgessler@gmail.com>
2019-07-22 17:50:03 -0700

    Remove nav from homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user
   skip_after_action :verify_authorized
   skip_after_action :verify_policy_scoped
+
+  def index
+    render layout: false
+  end
 end
