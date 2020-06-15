08c11fa375fb1623b1fcf679fac98136ed631969
Brad Gessler <bradgessler@gmail.com>
2020-06-14 17:04:04 -0700

    Raise an exception when appending children to an item when container=false
    This prevents items that are not containers from accumulating child items.
    
    I went down the route of creating a delegate for the association, as opposed
    to using a `before_add` callback, because the closure_tree gem didn't make
    it straight forward to add to the `children` association.
    
    If I had more time I'd patch the closure_tree gem with these hooks, perhaps
    `has_closure_tree before_add_child: :foo`, to the class configurator.
    
    A validation is also included in item that prevents a user from changing
    it from being a container with children, to not. The error will prompt them
    to first remove all of the children before they can make it an item.

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,6 +13,35 @@ class Item < ApplicationRecord
   validates :account, presence: true
   validates :user, presence: true
   validate :shelf_life_begin_less_than_end?
+  validate :convertable_from_container_to_item?
+  validate :parent_is_container?
+
+  # This was implemented this way because the clojure_tree gem
+  # needs to be patched to add a `before_add` callback to the `children`
+  # association that is capable of checking pre-conditions before a child
+  # is added to a node that might not allow for children to be added.
+  class ReadonlyAssociationDelegate
+    # Raise when trying to append a read-only association.
+    AppendError = Class.new(RuntimeError)
+
+    def initialize(association, readonly)
+      @association = association
+      @readonly = readonly
+    end
+
+    def <<(*args)
+      raise AppendError, "#{@association.inspect} is readonly" if @readonly
+      super(*args)
+    end
+
+    def method_missing(meth, *args)
+      @association.send(meth, *args)
+    end
+  end
+
+  def children
+    ReadonlyAssociationDelegate.new(super, !container)
+  end
 
   def find_or_create_label(text: name)
     label || create_label!(user: user, account: account, text: text)
@@ -45,6 +74,17 @@ class Item < ApplicationRecord
       if shelf_life_begin > shelf_life_end
         errors.add(:shelf_life_begin, "must be less than shelf life end")
       end
+    end
+
+    def convertable_from_container_to_item?
+      if not container? and children.exists?
+        errors.add(:container, "must have all items removed before it can be changed to not be a container")
+      end
+    end
+
+    def parent_is_container?
+      return if root?
+      errors.add :parent, "must be a container" if not parent.container?
     end
 
     def parse_date(date)
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:item) }
+  it { is_expected.to validate_presence_of(:name) }
+  describe "#container" do
+    context "true" do
+      subject { build(:item, container: true) }
+      it "#children#<< does not raise error" do
+        expect{subject.children << build(:item)}.to_not raise_error
+      end
+      it "#add_child does not raise error" do
+        expect{subject.add_child build(:item)}.to_not raise_error
+      end
+      context "changing to false" do
+        subject { build(:item, container: true) }
+        context "with children" do
+          before do
+            subject.children << build(:item)
+            subject.container = false
+          end
+          it { is_expected.to_not be_valid }
+        end
+        context "without children" do
+          before do
+            subject.container = false
+          end
+          it { is_expected.to be_valid }
+        end
+      end
+    end
+    context "false" do
+      subject { build(:item, container: false) }
+      it "#children#<< raises error" do
+        expect{subject.children << build(:item)}.to raise_error(Item::ReadonlyAssociationDelegate::AppendError)
+      end
+      it "#add_child raises error" do
+        expect{subject.add_child build(:item)}.to raise_error(Item::ReadonlyAssociationDelegate::AppendError)
+      end
+    end
+  end
+  describe "#parent" do
+    let(:root) { build(:item) }
+    context "root" do
+      subject { root }
+      it { is_expected.to be_root }
+      it { is_expected.to be_valid }
+    end
+    describe "validate parent" do
+      let(:root) { build(:item, container: parent_is_container) }
+      let(:child) { build(:item, parent: root)  }
+      subject { child }
+      context "is container" do
+        let(:parent_is_container) { true }
+        it { is_expected.to_not be_root }
+        it { is_expected.to be_valid }
+      end
+      context "is not container" do
+        let(:parent_is_container) { false }
+        it { is_expected.to_not be_root }
+        it { is_expected.to_not be_valid }
+      end
+    end
+  end
 end
