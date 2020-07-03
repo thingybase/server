fbde25e7a000d491a65ee637db38fa9c5faf4488
Brad Gessler <bradgessler@gmail.com>
2020-07-02 23:21:41 -0400

    Add better comments and private section to AccountsController

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -25,15 +25,17 @@ class AccountsController < ResourcesController
     redirect_to redirect_url
   end
 
-  def add_current_user_to_members
-    @account.members.create!(user: current_user) if @account.valid?
-  end
+  private
+    # After we create an account, add the current user that just created the account as the owner.
+    def add_current_user_to_members
+      @account.members.create!(user: current_user) if @account.valid?
+    end
 
-  def permitted_params
-    [:name]
-  end
+    def permitted_params
+      [:name]
+    end
 
-  def resource_scope
-    policy_scope.joins(:user)
-  end
+    def resource_scope
+      policy_scope.joins(:user)
+    end
 end
