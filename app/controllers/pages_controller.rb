cf35da32844dcb702f88da8bd2c9280d0daf03e8
Brad Gessler <bradgessler@gmail.com>
2021-03-20 08:16:15 -0700

    Fix footer and header layout rendering

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < Sitepress::SiteController
   skip_security!
   before_action :assign_title, only: :show
-  layout "page-with-footer"
+  layout "page"
 
   private
     def assign_title
