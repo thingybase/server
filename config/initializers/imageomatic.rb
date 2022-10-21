a54138f944b89ba982290c4ed9888924f66537b7
Brad Gessler <bradgessler@gmail.com>
2022-10-21 01:27:56 -0700

    Update imageomatic key

--- config/initializers/imageomatic.rb
+++ config/initializers/imageomatic.rb
@@ -6,7 +6,7 @@ Imageomatic.configure do |config|
     # IMAGEOMATIC_SECRET_KEY=
     # IMAGEOMATIC_PUBLIC_KEY=
     # ```
-    config.load_env!
+    config.load_env
   else
     config.secret_key = "development_secret_449d1d36c75e6df2e5074234274d"
     config.public_key = "development_public_a3b61d77a85233f9068bfd39c390"
