fd9c902a198df153e08c40cc116981b4bdb56a5b
Brad Gessler <bradgessler@gmail.com>
2022-11-30 22:02:35 -0800

    Enable pg_trgm extension to use trigram full text search indices

---
+++ db/migrate/20221201055922_enable_pg_triagram_extension.rb
@@ -0,0 +1,5 @@
+class EnablePgTriagramExtension < ActiveRecord::Migration[7.0]
+  def change
+    enable_extension "pg_trgm"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_090709) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_055922) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
