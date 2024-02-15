46de82171b37673782d58dc3ebe4629c241d2a41
Brad Gessler <bradgessler@gmail.com>
2024-02-15 10:41:57 -0800

    Get Labels working with Tabs

--- app/controllers/labels/base_controller.rb
+++ app/controllers/labels/base_controller.rb
@@ -4,6 +4,64 @@ module Labels
     include LabelsHelper
     include PreviewablePdf
 
+    include Superview::Actions
+
+    class Show < AccountLayout::Component
+      attr_writer :label
+
+      def title = @label.text
+      def icon = @label.icon
+      def subtitle
+        Breadcrumb do |it|
+          it.crumb { show(@account, :name) }
+          @label.item.ancestors.each do |item|
+            it.crumb { show(item, :name) }
+          end
+          it.crumb { show(@label.item, :name) }
+          it.crumb { "Label" }
+        end
+      end
+
+      def template
+        div class: "flex flex-col gap-8" do
+          Tab class: "tabs-boxed" do |it|
+            it.tab(label_standard_path(@label)) { "Standard" }
+            it.tab(label_jumbo_path(@label)) { "Jumbo" }
+            it.tab(label_code_path(@label)) { "Handwritten" }
+          end
+
+          div(class: "bg-neutral-500 w-fit p-8 rounded w-full min-h-96") do
+            img(src: url_for(format: :png), class: "mx-auto")
+          end
+        end
+      end
+
+      def action_template
+        LinkButton(url_for(format: :pdf), :primary) { "Print" }
+      end
+    end
+
+    def show
+      respond_to do |format|
+        format.html { render phlex, layout: false }
+        format.pdf do
+          send_data prawn_document.render, disposition: "inline", type: "application/pdf"
+        end
+        format.png do
+          prawn_png do |path|
+            send_file path, disposition: "inline", type: "image/png"
+          end
+        end
+      end
+    end
+
+    def phlex
+      super.tap do |view|
+        view.account = @account
+        view.user = current_user
+      end
+    end
+
     protected
       def uids
         params.fetch(:ids).split(",")
@@ -14,3 +72,4 @@ module Labels
       end
   end
 end
+# Path: app/controllers/labels/standard_controller.rb
--- app/views/components/account_component.rb
+++ app/views/components/account_component.rb
@@ -41,15 +41,17 @@ class AccountComponent < ApplicationComponent
       div(class: "flex flex-row") do
         sidebar_template
 
-        div(class: "m-4") do
+        div(class: "m-4 w-full") do
           section(class: "flex flex-col gap-4 p-4 md:gap-8 md:p-8") do
             div(class: "mb-4") { @notice } if @notice
             yield
           end
+
           footer class: "my-6 mx-auto sm:hidden text-center" do
             a(href: account_path(@account), class: "btn btn-ghost") { "Manage #{@account.name}" }
           end
         end
+
       end
     end
   end
