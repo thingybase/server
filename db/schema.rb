052d19d6f62562f9621868b2844592f19826a156
Brad Gessler <bradgessler@gmail.com>
2025-07-28 12:32:38 -0700

    Upgrade schema.rb to Rails 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_055922) do
+ActiveRecord::Schema[8.0].define(version: 2022_12_01_055922) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
   enable_extension "pgcrypto"
-  enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
     t.string "name", null: false
