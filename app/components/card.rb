d5ff267f567259939dc3f7a7ff56a72e999010b8
Brad Gessler <bradgessler@gmail.com>
2025-07-30 13:07:45 -0700

    Integrate card into dashboard

--- app/components/card.rb
+++ app/components/card.rb
@@ -1,15 +1,49 @@
 class Components::Card < Components::Base
+  class Text < self
+    def around_template(&)
+      super do
+        body(&) # Renders the `view_template`
+      end
+    end
+  end
+
+  def initialize(class: "bg-base-200 md:col-span-2")
+    @class = binding.local_variable_get("class")
+  end
+
   def around_template
     super do
-      div(class: "card p-4 rounded-xl shadow-lg") do
+      div(class: ["card", @class]) do
         yield # Renders the `view_template`
       end
     end
   end
 
-  def view_template
-    div do
-      yield if block_given?
+  def body
+    div(class: "card-body") do
+      yield # Renders the `view_template`
     end
+  end
+
+  class Header < Components::Base
+    def view_template(&)
+      hgroup(&)
+    end
+
+    def title(&)
+      h3(class: "card-title", &)
+    end
+
+    def subtitle(&)
+      h4(class: "font-semibold", &)
+    end
+  end
+
+  def header(&)
+    render Header.new(&)
+  end
+
+  def view_template
+    yield if block_given?
   end
 end
