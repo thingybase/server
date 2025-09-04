318c2b504bf22b797778957d254157d0797f6501
Brad Gessler <bradgessler@gmail.com>
2025-09-04 13:51:15 -0700

    Extract stimulus clipboard video

--- app/components/copy_email_template.rb
+++ app/components/copy_email_template.rb
@@ -8,17 +8,19 @@ class Components::CopyEmailTemplate < Components::Base
   end
 
   def view_template
-    div(class: "flex flex-col gap-2", data: { controller: "clipboard" }) do
-      label(for: "email_template", class: "text-lg font-bold") { "Email template" }
-      textarea(id: "email_template", name: "email_template", class: "textarea textarea-bordered w-full", readonly: true, data: { target: "clipboard.source" }, rows: 7) do
-        @data
-      end
-      div(class: "help") do
-        "Copy and paste this into your email app and send to people so they can request to join this account."
-      end
-      div(class: "flex flex-row gap-4") do
-        mail_to("", "Send email", subject: "Join my account on Thingybase", body: @data, class: "btn")
-        button(class: "btn btn-outline", data: { action: "clipboard#copy" }) { "Copy template" }
+    render Stimulus::Clipboard.new do |clipboard|
+      div(class: "flex flex-col gap-2") do
+        label(for: "email_template", class: "text-lg font-bold") { "Email template" }
+        clipboard.textarea(id: "email_template", name: "email_template", class: "textarea textarea-bordered w-full", readonly: true, rows: 7) do
+          @data
+        end
+        div(class: "help") do
+          "Copy and paste this into your email app and send to people so they can request to join this account."
+        end
+        div(class: "flex flex-row gap-4") do
+          mail_to("", "Send email", subject: "Join my account on Thingybase", body: @data, class: "btn")
+          clipboard.button(class: "btn btn-outline") { "Copy template" }
+        end
       end
     end
   end
---
+++ app/components/stimulus/clipboard.rb
@@ -0,0 +1,27 @@
+module Components
+  class Stimulus::Clipboard < Components::Base
+    def around_template
+      super do
+        div(data: { controller: "clipboard" }) do
+          yield
+        end
+      end
+    end
+
+    def source_data
+      { data: { target: "clipboard.source" } }
+    end
+
+    def copy_data
+      { data: { action: "clipboard#copy" } }
+    end
+
+    def textarea(**attributes, &)
+      super(**attributes.merge(source_data), &)
+    end
+
+    def button(**attributes, &)
+      super(**attributes.merge(copy_data), &)
+    end
+  end
+end
